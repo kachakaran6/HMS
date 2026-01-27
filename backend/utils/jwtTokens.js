29e8483e21bffcb276c9ad327afcffe2d67ce8c8
kachakaran6 <kachakaran06@gmail.com>
2026-01-27 17:47:12 +0530

    cookies issue

--- backend/utils/jwtTokens.js
+++ backend/utils/jwtTokens.js
@@ -1,18 +1,23 @@
 export const generateToken = (user, message, statusCode, res) => {
   const token = user.generateJsonWebToken();
   const cookieName = user.role === "admin" ? "admin_token" : "user_token";
+
   res
     .status(statusCode)
     .cookie(cookieName, token, {
+      httpOnly: true,
+
+      // ✅ REQUIRED FOR LIVE (Vercel + Render)
+      secure: true, // HTTPS only
+      sameSite: "None", // cross-site cookie
+
       expires: new Date(
         Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000,
       ),
-      httpOnly: true,
     })
     .json({
       success: true,
       message,
       user,
-      token,
     });
 };
